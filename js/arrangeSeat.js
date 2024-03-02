b273cabf29e70e556055c12f5650e110481ef080
MC <workchung1995@email.com>
2024-03-03 00:25:21 +0800

    update

---
+++ js/arrangeSeat.js
@@ -0,0 +1,203 @@
+var SS301, SS302, SS401, SS402
+SS301 = SS302 = SS401 = SS402 =
+    [[" ", 1, "AA", 2, "AA", 3, "AA", 4, "AA", 5, "AA", 6, "AA", 7],
+    [1, "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S"],
+    [2, "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S"],
+    [3, "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S"],
+    [4, "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S"],
+    [5, "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S"],
+    [6, "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S"]]
+
+var SS305, SS307
+SS305 = SS307 =
+    [[" ", 1, 2, "AA", 3, 4, "AA", 5, 6, "AA", 7, 8],
+    [1, "S", "S", "AA", "S", "S", "AA", "S", "S", "AA", "S", "S"],
+    [2, "S", "S", "AA", "S", "S", "AA", "S", "S", "AA", "S", "S"],
+    [3, "S", "S", "AA", "S", "S", "AA", "S", "S", "AA", "S", "S"],
+    [4, "S", "S", "AA", "S", "S", "AA", "S", "S", "AA", "S", "S"],
+    [5, "S", "S", "AA", "S", "S", "AA", "S", "S", "AA", "S", "S"],
+    [6, "S", "S", "AA", "S", "S", "AA", "S", "S", "AA", "S", "S"],
+    [7, "S", "S", "AA", "S", "S", "AA", "S", "S", "AA", "S", "S"],
+    [8, "S", "S", "AA", "S", "S", "AA", "S", "S", "AA", "S", "S"]]
+
+var SS404, SS405, SS406, SS407, SS506, SS507
+SS404 = SS405 = SS406 = SS407 = SS506 = SS507 =
+    [[" ", 1, "AA", 2, "AA", 3, "AA", 4, "AA", 5, "AA", 6, "AA", 7],
+    [1, "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S"],
+    [2, "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S"],
+    [3, "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S"],
+    [4, "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S"],
+    [5, "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S"],
+    [6, "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S"],
+    [7, "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S"],
+    [8, "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S", "AA", "S"]]
+
+var SS606, SS607, SS608, SS609
+SS606 = SS607 = SS608 = SS609 =
+    [[" ", 1, 2, "AA", 3, 4, "AA", 5, 6, "AA", 7, 8],
+    [1, "S", "S", "AA", "S", "S", "AA", "S", "S", "AA", "S", "S"],
+    [2, "S", "S", "AA", "S", "S", "AA", "S", "S", "AA", "S", "S"],
+    [3, "S", "S", "AA", "S", "S", "AA", "S", "S", "AA", "S", "S"],
+    [4, "S", "S", "AA", "S", "S", "AA", "S", "S", "AA", "S", "S"],
+    [5, "S", "S", "AA", "S", "S", "AA", "S", "S"]]
+
+var SS202, SS303, SS402, SS502
+SS202 = SS303 = SS402 = SS502 =
+    [[" ", 1, 2, "AA", 3, 4, 5, 6, 7, 8, "AA", 9, 10,],
+    [1, "S", "S", "AA", "S", "S", "S", "S", "S", "S", "AA", "S", "S"],
+    [2, "S", "S", "AA", "S", "S", "S", "S", "S", "S", "AA", "S", "AA"],
+    [3, "S", "S", "AA", "S", "S", "S", "S", "S", "S", "AA", "S", "S"],
+    [4, "S", "S", "AA", "S", "S", "S", "S", "S", "S", "AA", "S", "S"],
+    [5, "S", "S", "AA", "S", "S", "S", "S", "S", "S", "AA", "S", "S"],
+    [6, "S", "S", "AA", "S", "S", "S", "S", "S", "S", "AA", "S", "AA"],
+    [7, "AA", "S", "AA", "S", "S", "S", "S", "S", "S", "AA", "S", "AA"],
+    [8, "S", "S", "AA", "S", "S", "S", "S", "S", "S", "AA", "S", "S"],
+    [9, "S", "S", "AA", "S", "S", "S", "S", "S", "S", "AA", "S", "S"],
+    [10, "S", "S", "AA", "S", "S", "S", "S", "S", "S", "AA", "S", "S"],
+    [11, "S", "S", "AA", "S", "S", "S", "S", "S", "S", "AA", "S", "AA"],
+    [12, "S", "S", "AA", "S", "S", "S", "S", "S", "S", "AA", "S", "AA"],
+    ["AA", "AA", "AA", "AA", "AA", "AA", "AA", "AA", "AA", "AA", "AA", "AA", "AA"],
+    ["AA", "AA", "AA", "AA", "AA", "AA", "AA", "AA", "AA", "AA", "AA", "AA", "AA"],
+    [13, "S", "S", "AA", "S", "S", "S", "S", "S", "S", "AA", "S", "S"],
+    [14, "S", "S", "AA", "S", "S", "S", "S", "S", "S", "AA", "S", "S"],
+    [15, "AA", "AA", "AA", "S", "S", "S", "S", "S", "S", "AA", "AA", "AA"]]
+
+function createOption(text, value, disabled=false){
+    var option = document.createElement("option")
+    option.text = text
+    option.value = value
+    option.disabled = disabled
+    return option
+}
+
+function addOptions() {
+    const classroom = document.getElementById("classroom");
+    classroom.add(createOption("---------- 2F ----------", "", true))
+    classroom.add(createOption("SS202", "SS202"))
+    classroom.add(createOption("---------- 3F ----------", "", true))
+    classroom.add(createOption("SS301", "SS301"))
+    classroom.add(createOption("SS302", "SS302"))
+    classroom.add(createOption("SS303", "SS303"))
+    classroom.add(createOption("SS305", "SS305"))
+    classroom.add(createOption("SS307", "SS307"))
+    classroom.add(createOption("---------- 4F ----------", "", true))
+    classroom.add(createOption("SS401", "SS401"))
+    classroom.add(createOption("SS402", "SS402"))
+    classroom.add(createOption("SS403", "SS403"))
+    classroom.add(createOption("SS404", "SS404"))
+    classroom.add(createOption("SS405", "SS405"))
+    classroom.add(createOption("SS406", "SS406"))
+    classroom.add(createOption("SS407", "SS407"))
+    classroom.add(createOption("---------- 5F ----------", "", true))
+    classroom.add(createOption("SS502", "SS502"))
+    classroom.add(createOption("SS506", "SS506"))
+    classroom.add(createOption("SS507", "SS507"))
+    classroom.add(createOption("---------- 6F ----------", "", true))
+    classroom.add(createOption("SS606", "SS606"))
+    classroom.add(createOption("SS607", "SS607"))
+    classroom.add(createOption("SS608", "SS608"))
+    classroom.add(createOption("SS609", "SS609")) 
+}
+
+function getClassroom() {
+    const classroom = document.getElementById('classroom').value;
+    switch (classroom) {
+        case 'SS202':
+            return SS202;
+        case 'SS301':
+            return SS301;
+        case 'SS302':
+            return SS302;
+        case 'SS303':
+            return SS303;
+        case 'SS305':
+            return SS305;
+        case 'SS307':
+            return SS307;
+        case 'SS401':
+            return SS401;
+        case 'SS402':
+            return SS402;
+        case 'SS403':
+            return SS403;
+        case 'SS404':
+            return SS404;
+        case 'SS405':
+            return SS405;
+        case 'SS406':
+            return SS406;
+        case 'SS407':
+            return SS407;
+        case 'SS502':
+            return SS502;
+        case 'SS506':
+            return SS506;
+        case 'SS507':
+            return SS507;
+        case 'SS606':
+            return SS606;
+        case 'SS607':
+            return SS607;
+        case 'SS608':
+            return SS608;
+        case 'SS609':
+            return SS609;
+        default: return null;
+    }
+}
+
+function sample(array, size) {
+    const results = [],
+        sampled = {};
+    while (results.length < size && results.length < array.length) {
+        const index = Math.trunc(Math.random() * array.length);
+        if (!sampled[index]) {
+            results.push(array[index]);
+            sampled[index] = true;
+        }
+    }
+    return results;
+}
+
+function method1(sampleNames, classroom) {
+    var i = 0
+    for (var j = 0; j < classroom.length; j++) {
+        for (var k = 0; k < classroom[j].length; k++) {
+            if (classroom[j][k] === "S") {
+                classroom[j][k] = (i < sampleNames.length) ? sampleNames[i] : "";
+                i++
+            } else {
+                continue
+            }
+        }
+    }
+
+    return classroom;
+}
+
+function printTable() {
+    var table = document.getElementById('table1');
+    var divContents = document.getElementById("printElement").innerHTML;
+    var a = window.open('', '', 'height=500, width=500');
+    a.document.write('<html><head>');
+    a.document.write('<style type="text/css" media="print">tr, td, th {  -webkit-print-color-adjust: exact; -moz-print-color-adjust: exact; }</style>');
+    a.document.write('<style type="text/css" media="print">@page { size: landscape; }</style>');
+    a.document.write('<style>body, table {font-size: 20px;}</style > ');
+    a.document.write('<style>td, th {border: 1px solid black;text-align: center;padding: 8px;}</style > ');
+    a.document.write('<style>tr:nth-child(even) {background-color: #d4d4d4;}</style > ');
+    a.document.write('<style>.seat {justify-content: center;display: flex;}</style > ');
+    a.document.write('<style>.aisle {color: transparent; border: 1px solid white;background-color: white;}</style > ');
+    a.document.write('<style>.index {border: 1px solid white;background-color: white;}</style > ');
+    a.document.write('</head>');
+    a.document.write('<body>');
+    a.document.write('<div style="text-align:right; padding-right: 30px;">教室: ' + document.getElementById('classroom').value + '</div><br>');
+    a.document.write('<div style="text-align:center;"><h1>黑板</h1> </div><br>');
+    // a.document.write('<div style="text-align:center;padding-top:30px;"><h1>黑板</h1> </div><br>');
+    a.document.write(divContents);
+    // a.document.write(divContents);
+    a.document.write('<br><br><div style="text-align:left; padding-left: 30px;">應到人數: ' + numberOfStudents + '</div><br>');
+    a.document.write('<div style="text-align:left; padding-left: 30px;">實到人數: </div>');
+    a.document.write('</body></html>');
+    a.document.close();
+    a.print();
+}
